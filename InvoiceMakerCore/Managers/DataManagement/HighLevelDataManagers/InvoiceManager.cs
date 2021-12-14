e8b30e7a398460bec9e5890a233998ff8ddc6785
Andrzej Osowski <andrzejosowski93@gmail.com>
2021-12-14 12:41:57 +0100

    Added tests for invoice date checking

--- InvoiceMakerCore/Managers/DataManagement/HighLevelDataManagers/InvoiceManager.cs
+++ InvoiceMakerCore/Managers/DataManagement/HighLevelDataManagers/InvoiceManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using InvoiceMakerCore.Managers.DataManagement.DataBase;
 using InvoiceMakerCore.Models;
@@ -33,6 +34,20 @@ namespace InvoiceMakerCore.Managers.DataManagement.HighLevelDataManagers
             return _dataBase.Invoices;
         }
 
+        public IEnumerable<InvoiceModel> GetAllAfterDeadline()
+        {
+            return _dataBase.Invoices.Where(x =>
+                x.PaymentDate == new DateTime() &&
+                x.PaymentTerm < DateTime.Today);
+        }
+
+        public IEnumerable<InvoiceModel> GetAllWithTerm(int days)
+        {
+            return _dataBase.Invoices.Where(x =>
+                x.PaymentDate == new DateTime() &&
+                x.PaymentTerm < DateTime.Today.AddDays(days));
+        }
+
         public void Remove(int id)
         {
             var invoiceToBeRemoved = GetById(id);
@@ -45,6 +60,12 @@ namespace InvoiceMakerCore.Managers.DataManagement.HighLevelDataManagers
         public float GetInvoiceSum(InvoiceModel invoice)
         {
             return invoice.Products.Sum(x => x.Quantity * x.PricePerUnit);
+        }
+
+        public bool IsAfterDeadline(InvoiceModel invoice)
+        {
+            if (invoice.PaymentDate != new DateTime()) return false;
+            return invoice.PaymentTerm < DateTime.Today;
         }
     }
 }
--- InvoiceMakerTests/DataAccessTests/InvoiceDataTests.cs
+++ InvoiceMakerTests/DataAccessTests/InvoiceDataTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using InvoiceMakerCore.Annotations.Builders;
 using InvoiceMakerCore.Models;
@@ -101,6 +102,82 @@ namespace InvoiceMakerTests.DataAccessTests
             var dbInvoice = DataAccess.InvoiceManager.GetById(1);
             
             Assert.AreEqual(4 * number, DataAccess.InvoiceManager.GetInvoiceSum(dbInvoice));
+        }
+
+        [Test]
+        public void IsInvoiceAfterDeadlineTest()
+        {
+            var client = DataObjectsMock.MockClient(0);
+            var invoice_0 = DataObjectsMock.MockInvoice(client, 0);
+            var invoice_1 = DataObjectsMock.MockInvoice(client, 1);
+            invoice_1.PaymentDate = new DateTime();
+            invoice_1.PaymentTerm = DateTime.Today.AddDays(-5);
+
+            Assert.False(DataAccess.InvoiceManager.IsAfterDeadline(invoice_0));
+            Assert.True(DataAccess.InvoiceManager.IsAfterDeadline(invoice_1));
+        }
+
+        [Test]
+        public void GetAllInvoicesAfterDeadlineTest()
+        {
+            var client = DataObjectsMock.MockClient(0);
+            var invoice_0 = DataObjectsMock.MockInvoice(client, 0);
+            invoice_0.PaymentDate = new DateTime();
+            invoice_0.PaymentTerm = DateTime.Today;
+            var invoice_1 = DataObjectsMock.MockInvoice(client, 1);
+            invoice_1.PaymentDate = new DateTime();
+            invoice_1.PaymentTerm = DateTime.Today.AddDays(-1);
+            var invoice_2 = DataObjectsMock.MockInvoice(client, 2);
+            invoice_2.PaymentDate = new DateTime();
+            invoice_2.PaymentTerm = DateTime.Today.AddDays(-5);
+            var invoice_3 = DataObjectsMock.MockInvoice(client, 3);
+            var invoice_4 = DataObjectsMock.MockInvoice(client, 4);
+            
+            DataAccess.InvoiceManager.Add(invoice_0);
+            DataAccess.InvoiceManager.Add(invoice_1);
+            DataAccess.InvoiceManager.Add(invoice_2);
+            DataAccess.InvoiceManager.Add(invoice_3);
+            DataAccess.InvoiceManager.Add(invoice_4);
+            
+            Assert.AreEqual(5, DataAccess.InvoiceManager.GetAll().Count());
+            Assert.AreEqual(2, DataAccess.InvoiceManager.GetAllAfterDeadline().Count());
+        }
+
+        [TestCase(3)]
+        [TestCase(10)]
+        [TestCase(30)]
+        public void GetAllInvoicesWithDeadlineInDaysTest(int days)
+        {
+            var client = DataObjectsMock.MockClient(0);
+            var invoice_0 = DataObjectsMock.MockInvoice(client, 0);
+            invoice_0.PaymentDate = new DateTime();
+            invoice_0.PaymentTerm = DateTime.Today;
+            var invoice_1 = DataObjectsMock.MockInvoice(client, 1);
+            invoice_1.PaymentDate = new DateTime();
+            invoice_1.PaymentTerm = DateTime.Today.AddDays(3);
+            var invoice_2 = DataObjectsMock.MockInvoice(client, 2);
+            invoice_2.PaymentDate = new DateTime();
+            invoice_2.PaymentTerm = DateTime.Today.AddDays(10);
+            var invoice_3 = DataObjectsMock.MockInvoice(client, 3);
+            invoice_3.PaymentDate = new DateTime();
+            invoice_3.PaymentTerm = DateTime.Today.AddDays(25);
+            var invoice_4 = DataObjectsMock.MockInvoice(client, 4);
+            
+            DataAccess.InvoiceManager.Add(invoice_0);
+            DataAccess.InvoiceManager.Add(invoice_1);
+            DataAccess.InvoiceManager.Add(invoice_2);
+            DataAccess.InvoiceManager.Add(invoice_3);
+            DataAccess.InvoiceManager.Add(invoice_4);
+            
+            Assert.AreEqual(5, DataAccess.InvoiceManager.GetAll().Count());
+            var expected = days switch
+            {
+                3 => 1,
+                10 => 2,
+                30 => 4
+            };
+
+            Assert.AreEqual(expected, DataAccess.InvoiceManager.GetAllWithTerm(days).Count());
         }
     }
 }
